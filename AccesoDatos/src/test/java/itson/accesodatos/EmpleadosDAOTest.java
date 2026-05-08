713626c81a8c2b03d8ea4e4452cb23ccaf5dd21e
Hector232 <jesushector201006@gmail.com>
2026-05-08 08:44:38 -0700

    Test EmpleadosDaoTest

--- AccesoDatos/src/test/java/itson/accesodatos/EmpleadosDAOTest.java
+++ AccesoDatos/src/test/java/itson/accesodatos/EmpleadosDAOTest.java
@@ -8,6 +8,8 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import itson.entidades.Empleado;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
@@ -21,22 +23,102 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Zaira
  */
 public class EmpleadosDAOTest {
+
     IAccesoDatos<Empleado> dao = EmpleadosDAO.getInstance();
-    
+
     public EmpleadosDAOTest() {
     }
 
     @Test
-    public void testCrear(){
-        Empleado empleado = new Empleado(
-                "Zaira",
-                "Barajas",
-                "Diaz"
-        );
-        
-        Empleado empleadoCreado = dao.crear(empleado);
-        assertNotNull(empleadoCreado);
-        assertNotNull(empleadoCreado.getId());
-    }
+    public void testCrear() {
+//        Empleado empleado = new Empleado(
+//                "Zaira",
+//                "Barajas",
+//                "Diaz"
+//        );
+//
+//        Empleado empleadoCreado = dao.crear(empleado);
+//        assertNotNull(empleadoCreado);
+//        assertNotNull(empleadoCreado.getId());
+//
+//        empleado = new Empleado(
+//                "Ramses",
+//                "Contreras",
+//                "Avila"
+//        );
+//
+//        empleadoCreado = dao.crear(empleado);
+//        assertNotNull(empleadoCreado);
+//        assertNotNull(empleadoCreado.getId());
+//
+//        empleado = new Empleado(
+//                "Josmara",
+//                "Quintana",
+//                "Benitez"
+//        );
+//
+//        empleadoCreado = dao.crear(empleado);
+//        assertNotNull(empleadoCreado);
+//        assertNotNull(empleadoCreado.getId());
+//
+//        empleado = new Empleado(
+//                "Hector",
+//                "Flores",
+//                "Montoya"
+//        );
+//
+//        empleadoCreado = dao.crear(empleado);
+//        assertNotNull(empleadoCreado);
+//        assertNotNull(empleadoCreado.getId());
+//
     
+
+////        empleado = new Empleado(
+////                "Juan Carlos",
+////                "Pérez",
+////                "García"
+////        );
+////        empleado.setCodigoPostal(03100);
+////        empleado.setCurp("PEGJ920515HDFRRN01");
+////        empleado.setFechaNacimiento(LocalDate.of(1992, Month.MAY, 15));
+////        empleado.setRfc("PEGJ9205151A2");
+////        empleado.setCalle("Av. Siempre Viva");
+////        empleado.setNss("12345678901");
+////        empleado.setColonia("Del Valle");
+////
+////        empleadoCreado = dao.crear(empleado);
+////        assertNotNull(empleadoCreado);
+////        assertNotNull(empleadoCreado.getId());
+//        empleado = new Empleado(
+//                "Fulanito",
+//                "Perez",
+//                ""
+//        );
+//
+//        empleadoCreado = dao.crear(empleado);
+//        assertNotNull(empleadoCreado);
+//        assertNotNull(empleadoCreado.getId());
+//
+//        empleado = new Empleado(
+//                "Manganito",
+//                "Juárez",
+//                ""
+//        );
+//
+//        empleadoCreado = dao.crear(empleado);
+//        assertNotNull(empleadoCreado);
+//        assertNotNull(empleadoCreado.getId());
+//
+//        empleado = new Empleado(
+//                "Ana Alicia",
+//                "Armenta",
+//                ""
+//        );
+//
+//        empleadoCreado = dao.crear(empleado);
+//        assertNotNull(empleadoCreado);
+//        assertNotNull(empleadoCreado.getId());
+
+    }
+
 }
